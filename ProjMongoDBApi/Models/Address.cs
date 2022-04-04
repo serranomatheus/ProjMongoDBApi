77bcce359eb6cfa0a96260be62f31310cd8639ce
serranomatheus <serranomatheus2@icloud.com>
2022-04-04 11:01:25 -0300

    Adicionar arquivos de projeto.

---
+++ ProjMongoDBApi/Models/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjMongoDBApi.Model
+{
+    public class Address
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        #endregion
+    }
+}
